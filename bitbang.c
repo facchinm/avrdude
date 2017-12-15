952c2af9486880ccdfac68171e11541fad1e4b22
Martino Facchin <m.facchin@arduino.cc>
2017-12-15 13:52:25 +0100

    avrdude-6.3-silent_bitbang_cmd

--- bitbang.c
+++ bitbang.c
@@ -331,7 +331,7 @@ int bitbang_cmd(PROGRAMMER * pgm, const unsigned char *cmd,
     res[i] = bitbang_txrx(pgm, cmd[i]);
   }
 
-    if(verbose >= 2)
+    if(verbose > 4)
 	{
         avrdude_message(MSG_NOTICE2, "bitbang_cmd(): [ ");
         for(i = 0; i < 4; i++)
